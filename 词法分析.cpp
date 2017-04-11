6d05ae14c079c3fb15a0c425b3a2f696e15b0309
cocolord <cocolord@users.noreply.github.com>
2017-04-11 19:01:42 +0800

    Update 词法分析.cpp

--- 词法分析.cpp
+++ 词法分析.cpp
@@ -11,6 +11,33 @@ const string Reserverd[20] = {"if","then","else","end","repeat","until","read","
 
 string buffer[100];
 
+bool lookupReserved(string s)
+{
+    for(int i = 0;i<8;i++)
+    {
+        if(s==Reserverd[i])
+            return true;
+    }
+    return false;
+}
+
+bool isSymbol(string s)
+{
+    if(s=="<"||s==">"||s=="="||s==":="||s=="*"||s=="/"||s==";"||s=="("||s==")")
+        return true;
+    return false;
+}
+bool isNum(string s)
+{
+    for(int i = 0;i<s.size();i++)
+    {
+        if(isdigit(s[i]))
+            ;
+        else
+            return false;
+    }
+    return true;
+}
 int processLine(string buffer[],string s)
 {
 	int cur = 0;
@@ -28,25 +55,41 @@ void ReadandWrite()
 {
     ifstream fin("test.tiny");  
     string s;
+    bool incomment = false;
 	int cur = 0;
+    ofstream out("out.text",ios::app);
+    out <<"*****************************************\nTINY Lexical Analazer Result:\n*****************************************\n";
 	while(getline(fin,s))
 	{
 		int cur = processLine(buffer,s);
-		ofstream out("out.text",ios::app);
 		for(int i = 0;i<cur;i++)
 		{
-			cout<<lineno;
-			out<<buffer[i]<<'\n';
+            if(lookupReserved(buffer[i]))
+            {
+			    out<<lineno<<":     ";
+                out<<"Reserved Words: ";
+		        out<<buffer[i]<<'\n';
+            }
+            else if(isSymbol(buffer[i]))
+            {
+			    out<<lineno<<":     ";
+			    out<<buffer[i]<<'\n';
+            }
+            else if(isNum(buffer[i]))
+            {
+			    out<<lineno<<":     ";
+                out<<"NUM, val = "<<buffer[i]<<'\n';
+            }
+            else 
+            {
+			    out<<lineno<<":     ";
+                out<<"ID, name = "<<buffer[i]<<'\n';
+            }
 		}
 		lineno++;
 	}
 }
 
-//自动机
-void getToken()
-{
-
-}
 
 int main()
 {
